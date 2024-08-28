c517a4e2d79a894634b2826dd59fd52ced361c1d
Thais <thais.gebel@gm2dev.com>
2024-08-28 09:43:54 -0300

    feat(results):fix crown js, fix style css container, fix image html

--- resultsScreen/results.js
+++ resultsScreen/results.js
@@ -3,21 +3,24 @@ let correctNumber = document.querySelector(".correct-number");
 let wrongNumber = document.querySelector(".wrong-number");
 let skipNumber = document.querySelector(".skip-number");
 
-function numberOfQuestions() {
-  let correctAnswers = localStorage.getItem("correctAnswers");
-  let wrongAnswers = localStorage.getItem("wrongAnswers");
-  let skipAnswers = localStorage.getItem("skipAnswers");
+// let correctAnswers = localStorage.getItem("correctAnswers");
+// let wrongAnswers = localStorage.getItem("wrongAnswers");
+// let skipAnswers = localStorage.getItem("skipAnswers");
+let correctAnswers = 8;
+let wrongAnswers = 34;
+let skipAnswers = 34;
 
-  correctNumber.textContent = correctAnswers;
-  wrongNumber.textContent = wrongAnswers;
-  skipNumber.textContent = skipAnswers;
+function numberOfQuestions(correct, wrong, skip) {
+  correctNumber.textContent = correct;
+  wrongNumber.textContent = wrong;
+  skipNumber.textContent = skip;
 }
 
-numberOfQuestions();
+numberOfQuestions(correctAnswers, wrongAnswers, skipAnswers);
 
 crown.style.display = "none";
 
-crown.addEventListener("load", function () {
+window.addEventListener("load", function () {
   if (correctAnswers >= 7) {
     crown.style.display = "block";
   }
