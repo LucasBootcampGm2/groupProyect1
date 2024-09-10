7b2b6baaf63b09cee6e553b4bd355028009154b8
Thais <thais.gebel@gm2dev.com>
2024-09-10 12:31:41 -0300

    feat(answersExplain): add load answers, next and previous button and no more answers logic

--- answersExplained/answersExplained.js
+++ answersExplained/answersExplained.js
@@ -1,4 +1,19 @@
+// if (alreadyAsked.includes(finalQuestions[index].question === false)) {
+//   alreadyAsked.push(finalQuestions[index].question);
+// }
+
 // // let answer = localStorage.getItem("answer");
+
+let answers = document.getElementById("container-answers");
+let containerContent = document.getElementById("container-content");
+let numberQuestion = document.querySelector(".number-question");
+let textQuestion = document.querySelector(".text-question");
+let correctAnswer = document.querySelector(".correct-answer");
+let explanationAnswer = document.querySelector(".explanation-answer");
+let youAnswer = document.querySelector(".you-answer");
+
+let buttonNextAnswers = document.getElementById("button-next-answer");
+let buttonPrevAnswers = document.getElementById("button-prev-answer");
 
 let selectedCategory = "science";
 let selectedDifficulty = "easy";
@@ -14,85 +29,100 @@ function getDifficulty(questionsByCategory) {
 }
 
 let finalQuestions = getDifficulty(getCategory());
-let question = finalQuestions[1].question;
 
-let buttonNextAnswers = document.getElementById("button-next-answer");
-let answers = document.getElementById("container-answers");
-
-function loadAnswersButton(finalQuestions, finalAnswer) {
-  let alreadyAsked = [];
-  let index = 0;
-
-  if (alreadyAsked.includes(finalQuestions[index].question === false)) {
-    alreadyAsked.push(finalQuestions[index].question);
-  }
-
-  let divContent = document.createElement("div");
-  divContent.classList.add("div-content");
-
-  let title = document.createElement("h3");
-  title.textContent = `Question #${index + 1}`;
-
-  let question = document.createElement("p");
-  question.textContent = `Pregunta: ${finalQuestions[index].question}`;
-
-  let correctAnswer = document.createElement("p");
-  correctAnswer.textContent = `Respuesta correcta: ${finalQuestions[index].correct}`;
-
-  let explanation = document.createElement("p");
-  explanation.textContent = `Explicacion: ${finalQuestions[index].explanation}`;
-
-  let youAnswer = document.createElement("p");
-  youAnswer.textContent = `Tu respuesta: ${finalAnswer}`;
-
-  if (finalAnswer === "Correct") {
-    youAnswer.style.color = "green";
-    placeWhereTheAnswerIs = answers;
-  } else if (finalAnswer === "Wrong") {
-    youAnswer.style.color = "red";
-    placeWhereTheAnswerIs = answers;
-  } else if (finalAnswer === "Skip") {
-    youAnswer.style.color = "grey";
-    placeWhereTheAnswerIs = answers;
-  }
-
-  placeWhereTheAnswerIs.append(divContent);
-  divContent.append(title);
-  divContent.append(question);
-  divContent.append(correctAnswer);
-  divContent.append(explanation);
-  divContent.append(youAnswer);
-
-  index++;
-
-  buttonNextAnswers.addEventListener("click", function () {
-    if (alreadyAsked.includes(finalQuestions[index].question === false)) {
-      alreadyAsked.push(finalQuestions[index].question);
-    }
-
-    title.textContent = `Question #${index + 1}`;
-
-    question.textContent = `Pregunta: ${finalQuestions[index].question}`;
-
-    correctAnswer.textContent = `Respuesta correcta: ${finalQuestions[index].correct}`;
-
-    explanation.textContent = `Explicacion: ${finalQuestions[index].explanation}`;
-
-    youAnswer.textContent = `Tu respuesta: ${finalAnswer}`;
-
-    if (finalAnswer === "Correct") {
-      youAnswer.style.color = "green";
-      placeWhereTheAnswerIs = answers;
-    } else if (finalAnswer === "Wrong") {
-      youAnswer.style.color = "red";
-      placeWhereTheAnswerIs = answers;
-    } else if (finalAnswer === "Skip") {
-      youAnswer.style.color = "grey";
-      placeWhereTheAnswerIs = answers;
-    }
-
-    index++;
-  });
+function appendInformation() {
+  answers.append(containerContent);
+  containerContent.append(numberQuestion);
+  containerContent.append(textQuestion);
+  containerContent.append(correctAnswer);
+  containerContent.append(explanationAnswer);
+  containerContent.append(youAnswer);
 }
 
-loadAnswersButton(finalQuestions, "Wrong");
+let finalAnswer = "Correct";
+let index = 0;
+
+function nextAnswer() {
+  if (index >= 0 && index < finalQuestions.length) {
+    index++;
+  }
+}
+
+function prevAnswer() {
+  if (index > 0 && index <= finalQuestions.length) {
+    index--;
+  }
+}
+
+function showInformation() {
+  numberQuestion.textContent = `Question #${index + 1}`;
+  textQuestion.textContent = `Pregunta: ${finalQuestions[index].question}`;
+  correctAnswer.textContent = `Respuesta correcta: ${finalQuestions[index].correct}`;
+  explanationAnswer.textContent = `Explicacion: ${finalQuestions[index].explanation}`;
+  youAnswer.textContent = `Tu respuesta: ${finalAnswer}`;
+}
+
+function addOrQuitClass() {
+  containerContent.classList.toggle("flex");
+  numberQuestion.classList.toggle("center");
+  textQuestion.classList.toggle("hide");
+  correctAnswer.classList.toggle("hide");
+  explanationAnswer.classList.toggle("hide");
+  youAnswer.classList.toggle("hide");
+  buttonNextAnswers.classList.toggle("hide");
+}
+
+function loadAnswers() {
+  console.log({ index, finalQuestions });
+  if (index === 0) {
+    buttonPrevAnswers.style.display = "none";
+    showInformation();
+    appendInformation();
+  } else if (index < finalQuestions.length) {
+    buttonPrevAnswers.style.display = "block";
+    showInformation();
+    appendInformation();
+  } else if (index === 10) {
+    addOrQuitClass();
+    numberQuestion.textContent = "NO HAY MAS PREGUNTAS :((";
+  }
+}
+
+function loadPrevAnswers() {
+  console.log({ index, finalQuestions });
+  if (index === 0) {
+    buttonPrevAnswers.style.display = "none";
+    buttonNextAnswers.style.display = "block";
+  } else if (index > 0 && index < 9) {
+    buttonNextAnswers.style.display = "block";
+  } else if (index === 9) {
+    addOrQuitClass();
+  }
+  showInformation();
+  appendInformation();
+}
+
+buttonNextAnswers.addEventListener("click", function () {
+  nextAnswer();
+  loadAnswers();
+});
+
+buttonPrevAnswers.addEventListener("click", function () {
+  prevAnswer();
+  loadPrevAnswers();
+});
+
+function colorAnswer() {
+  if (finalAnswer === "Correct") {
+    youAnswer.style.color = "green";
+  } else if (finalAnswer === "Wrong") {
+    youAnswer.style.color = "red";
+  } else if (finalAnswer === "Skip") {
+    youAnswer.style.color = "grey";
+  }
+}
+
+window.addEventListener("load", function () {
+  loadAnswers();
+  colorAnswer();
+});
