adbf37689ca911b569895a4a453a15d2e02ac17a
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-09-13 12:13:39 -0300

    fix(test.css): wrong header transitions

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
 let button = document.querySelector(".buttonSubmit")
-let header = document.querySelector(".headerT")
+let h1 = document.getElementById("h1")
 let main = document.querySelector(".main")
 let modal = document.getElementById("modal")
+let header = document.querySelector('.header')
 
 button.addEventListener("click", function(){
     document.getElementById('modal').classList.remove('show')
     setTimeout(function(){
         main.style.display = "flex";
+        h1.style.display = "flex";
         header.style.display = "flex";
         modal.style.display = "none";
         
