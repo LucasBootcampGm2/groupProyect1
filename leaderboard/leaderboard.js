617c4da558ebe5552ef42b26f298c2a5dcc97fd0
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-09-13 15:30:46 -0300

    feat(leaderboard.js): create an exaple of a function to complete the leaderboard

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,23 +1,39 @@
-let button = document.querySelector(".buttonSubmit")
-let h1 = document.getElementById("h1")
-let main = document.querySelector(".main")
-let modal = document.getElementById("modal")
-let header = document.querySelector('.header')
-
-button.addEventListener("click", function(){
-    document.getElementById('modal').classList.remove('show')
-    setTimeout(function(){
-        main.style.display = "flex";
-        h1.style.display = "flex";
-        header.style.display = "flex";
-        modal.style.display = "none";
-        
-    }, 1000)
+let button = document.querySelector(".buttonSubmit");
+let h1 = document.getElementById("h1");
+let main = document.querySelector(".main");
+let modal = document.getElementById("modal");
+let header = document.querySelector(".header");
+let leaderboard = [
+  // {
+  //     userName: ,
+  //     correctAnswers: ,
+  //     wrongAnswers: ,
+  //     skipedAnswers: ,
+  //     totalPoints: ,
+  // }
+];
+localStorage.setItem("leaderboard", "[]");
+button.addEventListener("click", function () {
+  document.getElementById("modal").classList.remove("show");
+  setTimeout(function () {
+    main.style.display = "flex";
+    h1.style.display = "flex";
+    header.style.display = "flex";
+    modal.style.display = "none";
+  }, 1000);
 });
 
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById("modal").classList.add("show");
-    }, 200); 
-};
+// function completeLeaderTable(user, corrects, wrong, skip) {
+//   leaderboard.push({
+//     userName: user,
+//     correctAnswers: corrects,
+//     wrongAnswers: wrong,
+//     skipedAnswers: skip,
+//   });
+// }
 
+window.onload = function () {
+  setTimeout(function () {
+    document.getElementById("modal").classList.add("show");
+  }, 200);
+};
