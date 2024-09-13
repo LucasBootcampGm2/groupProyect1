819efc95e8d6861baac63c7252aff4772743e092
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-09-13 12:25:59 -0300

    feat: refactor leaderboard page

--- leaderboard/script.js
+++
@@ -1,4 +0,0 @@
-let user = localStorage.getItem("userName") //consultar con thais
-let score = localStorage.getItem("score") //consultar con thais
-let ranking = localStorage.getitem("ranking") //consultar con thais
-
