5cc1b2f91715350573da955faba400227f5268ed
Thais <thais.gebel@gm2dev.com>
2024-09-16 10:31:22 -0300

    feat(resultsScreen): add score specification

--- resultsScreen/results.js
+++ resultsScreen/results.js
@@ -5,10 +5,9 @@ let correctAnswers = localStorage.getItem("correctAnswers");
 let wrongAnswers = localStorage.getItem("wrongAnswers");
 let skipedAnswers = localStorage.getItem("skipedAnswers");
 
-
 function numberOfQuestions(correct, wrong, skip) {
-  document.querySelector(".correct-number").textContent+ correct
-  document.querySelector(".wrong-number").textContent = wrong
+  document.querySelector(".correct-number").textContent + correct;
+  document.querySelector(".wrong-number").textContent = wrong;
   document.querySelector(".skip-number").textContent = skip;
 }
 
@@ -18,13 +17,17 @@ function finalScore(correct, skip) {
   let finalScore = 0;
 
   finalScore += correct * 120;
-  finalScore -= skip * -(10);
+  finalScore -= skip * -10;
 
   score.textContent = `${finalScore}/100`;
 }
 
 window.addEventListener("load", function () {
-  numberOfQuestions(localStorage.getItem("correctAnswers"), localStorage.getItem("wrongAnswers"), localStorage.getItem("skipedAnswers"));
+  numberOfQuestions(
+    localStorage.getItem("correctAnswers"),
+    localStorage.getItem("wrongAnswers"),
+    localStorage.getItem("skipedAnswers")
+  );
   finalScore(correctAnswers, skipedAnswers);
   if (correctAnswers >= 7) {
     crown.style.display = "block";
