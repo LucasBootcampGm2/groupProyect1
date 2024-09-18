56cc6884f8499cafd4a740d2707f7dfa4fd87e1b
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-09-18 11:52:00 -0300

    feat(leaderboard): add table functionability

--- resultsScreen/results.js
+++ resultsScreen/results.js
@@ -1,35 +1,67 @@
 let crown = document.querySelector(".fa-crown");
 let score = document.querySelector(".score");
-
-let correctAnswers = localStorage.getItem("correctAnswers");
-let wrongAnswers = localStorage.getItem("wrongAnswers");
-let skipedAnswers = localStorage.getItem("skipedAnswers");
-
+let user = JSON.parse(localStorage.getItem("user"));
+function getCorrecAnswers() {
+  let correctAnswers = user.correctAnswers;
+  return correctAnswers;
+}
+function getWrongAnswers() {
+  let wrongAnswers = user.wrongAnswers;
+  return wrongAnswers;
+}
+function getSkippedAnswers() {
+  let skippedAnswers = user.skippedAnswers;
+  return skippedAnswers;
+}
 function numberOfQuestions(correct, wrong, skip) {
   document.querySelector(".correct-number").textContent + correct;
   document.querySelector(".wrong-number").textContent = wrong;
   document.querySelector(".skip-number").textContent = skip;
 }
-
 crown.style.display = "none";
-
-function finalScore(correct, wrong) {
-  let finalScore = 0;
-
-  finalScore += correct * 120;
-  finalScore -= wrong * -10;
-
-  score.textContent = `${finalScore}/100`;
+function substractPoints(wrong) {
+  let substract;
+  switch (localStorage.getItem("difficulty")) {
+    case "easy":
+      substract = 2;
+      break;
+    case "medium":
+      substract = 5;
+      break;
+    case "hard":
+      substract = 7;
+      break;
+  }
+  return substract;
 }
-
+function addPoints() {
+  let multiply;
+  switch (localStorage.getItem("difficulty")) {
+    case "easy":
+      multiply = 5;
+      break;
+    case "medium":
+      multiply = 10;
+      break;
+    case "hard":
+      multiply = 15;
+      break;
+  }
+  return multiply;
+}
+function finalScore(correct, wrong) {
+  let totalPoints = addPoints() * correct - substractPoints() * wrong;
+  if (totalPoints < 0) {
+    totalPoints = 0;
+  }
+  score.textContent = totalPoints;
+  user.totalPoints = totalPoints;
+  localStorage.setItem("user", JSON.stringify(user));
+}
 window.addEventListener("load", function () {
-  numberOfQuestions(
-    localStorage.getItem("correctAnswers"),
-    localStorage.getItem("wrongAnswers"),
-    localStorage.getItem("skipedAnswers")
-  );
-  finalScore(correctAnswers, skipedAnswers);
-  if (correctAnswers >= 7) {
+  numberOfQuestions(getCorrecAnswers(), getWrongAnswers(), getSkippedAnswers());
+  finalScore(getCorrecAnswers(), getWrongAnswers());
+  if (getCorrecAnswers() >= 7) {
     crown.style.display = "block";
   }
 });
