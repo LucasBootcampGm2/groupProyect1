098641fdb42e302b4389516a5d39b821d5750247
Lucas <lcirilloberardi@alumno.huergo.edu.ar>
2024-09-20 19:53:32 -0300

    feat(home): add spans for show selection

--- home/home.js
+++ home/home.js
@@ -1,50 +1,60 @@
-import { questions } from "../questionBank.js";
-let modal = document.getElementById("modal");
-let button = document.querySelector(".buttonSubmit");
-let header = document.querySelector(".header");
-let sectionQuizMode = document.getElementById("section-quiz-mode");
+import { questions } from "../questionBank.js"
+let modal = document.getElementById("modal")
+let button = document.querySelector(".buttonSubmit")
+let header = document.querySelector(".header")
+let sectionQuizMode = document.getElementById("section-quiz-mode")
+let containerSpans = document.querySelector(".container-spans")
+let userObject = {}
 
-let userObject = {};
+let categories = Object.keys(questions)
 
-let categories = Object.keys(questions);
-
-console.log(categories);
-let difficultyButton = document.getElementById("difficulty-button");
+console.log(categories)
 
 function addUserName() {
-  let username = document.getElementById("username");
-  userObject.userName = username.value;
-  console.log(userObject);
-  localStorage.setItem("user", JSON.stringify(userObject));
+  let username = document.getElementById("username")
+  userObject.userName = username.value
+  console.log(userObject)
+  localStorage.setItem("user", JSON.stringify(userObject))
 }
 
 button.addEventListener("click", function () {
-  document.getElementById("modal").classList.remove("show");
-  console.log(addUserName());
+  modal.classList.remove("show")
+  header.classList.remove("show")
+  console.log(addUserName())
+  addCategoryButtons()
   setTimeout(function () {
-    main.style.display = "block";
-    modal.style.display = "none";
-  }, 1000);
-});
+    main.style.display = "block"
+    document
+      .querySelectorAll(".quiz-category-buttons")
+      .forEach(function (buttons) {
+        buttons.classList.add("show")
+      })
+    modal.style.display = "none"
+    header.style.display = "none"
+    containerSpans.style.display = "flex"
+  }, 1000)
+
+  setCategory()
+})
 
 function saveDifficulties() {
-  let difficulties = {};
+  let difficulties = {}
   categories.forEach(function (category) {
-    difficulties[category] = Object.keys(questions[category]);
-  });
-  return difficulties;
+    difficulties[category] = Object.keys(questions[category])
+  })
+  return difficulties
 }
-let difficulties = saveDifficulties();
-console.log(difficulties);
+let difficulties = saveDifficulties()
+console.log(difficulties)
 
 function getElementByQuery(element) {
-  return document.querySelectorAll(element);
+  return document.querySelectorAll(element)
 }
 
 function createNewClasses(element, classes) {
   classes.forEach(function (newClass) {
-    element.classList.add(newClass);
-  });
+    element.classList.add(newClass)
+  })
 }
 
 function createNewAttributes(element, attributes) {
@@ -52,34 +62,34 @@ function createNewAttributes(element, attributes) {
     element.setAttribute(
       newAttribute.attributeName,
       newAttribute.attributeValue
-    );
-  });
+    )
+  })
 }
 
 function addTextToElement(element, text) {
-  element.textContent = text;
+  element.textContent = text
 }
 
 function createCompleteElement(element, classes, attributes, text) {
-  let newElement = document.createElement(element);
-  createNewClasses(newElement, classes);
-  createNewAttributes(newElement, attributes);
-  addTextToElement(newElement, text);
-  return newElement;
+  let newElement = document.createElement(element)
+  createNewClasses(newElement, classes)
+  createNewAttributes(newElement, attributes)
+  addTextToElement(newElement, text)
+  return newElement
 }
 
 function addCategoryButtons() {
   categories.forEach(function (category) {
     let newButton = createCompleteElement(
       "button",
-      ["quiz-category-button", "quiz-button"],
+      ["quiz-category-buttons", "quiz-button"],
       [
         {
           attributeName: "id",
           attributeValue: `${category.toLowerCase()}-button`,
         },
       ]
-    );
+    )
 
     let newImg = createCompleteElement(
       "img",
@@ -98,20 +108,20 @@ function addCategoryButtons() {
           attributeValue: `${category.toLowerCase()}Img`,
         },
       ]
-    );
+    )
 
-    let newH4 = createCompleteElement("h4", ["quiz-h4"], [""], category);
-
-    newButton.append(newImg);
-    newButton.append(newH4);
-    sectionQuizMode.append(newButton);
-  });
+    let newH4 = createCompleteElement("h4", ["quiz-h4"], [""], category)
+    console.log(typeof newH4.textContent)
+    newButton.append(newImg)
+    newButton.append(newH4)
+    sectionQuizMode.append(newButton)
+  })
 }
 
 function addDifficultyButtons() {
-  let categorySelected = localStorage.getItem("category");
-  let categoryDifficulties = Object.values(difficulties[categorySelected]);
-  console.log(difficulties[categorySelected]);
+  let categorySelected = localStorage.getItem("category")
+  let categoryDifficulties = Object.values(difficulties[categorySelected])
+  console.log(difficulties[categorySelected])
 
   categoryDifficulties.forEach(function (categoryDifficulty) {
     let newButton = createCompleteElement(
@@ -123,7 +133,7 @@ function addDifficultyButtons() {
           attributeValue: `${categoryDifficulty}-button`,
         },
       ]
-    );
+    )
 
     let newImg = createCompleteElement(
       "img",
@@ -142,97 +152,84 @@ function addDifficultyButtons() {
           attributeValue: `${categoryDifficulty}Img`,
         },
       ]
-    );
+    )
 
-    newButton.append(newImg);
+    newButton.append(newImg)
 
-    let newH4 = createCompleteElement(
-      "h4",
-      ["quiz-h4"],
-      [],
-      categoryDifficulty
-    );
+    let newH4 = createCompleteElement("h4", ["quiz-h4"], [], categoryDifficulty)
 
-    newButton.append(newH4);
+    newButton.append(newH4)
 
-    sectionQuizMode.append(newButton);
-  });
+    sectionQuizMode.append(newButton)
+  })
 }
 
-function removeDifficultyButtons() {
+function appearDifficultyButtons() {
   getElementByQuery(".quiz-difficulty-buttons").forEach(function (difficulty) {
-    difficulty.remove();
-  });
+    difficulty.style.display = "block"
+  })
+}
+
+function disappearDifficultyButtons() {
+  getElementByQuery(".quiz-difficulty-buttons").forEach(function (difficulty) {
+    difficulty.remove()
+  })
 }
 
 function appearCategoryButtons() {
-  getElementByQuery(".quiz-category-button").forEach(function (category) {
-    sectionQuizMode.style.display = "flex";
-    category.style.display = "block";
-  });
+  getElementByQuery(".quiz-category-buttons").forEach(function (category) {
+    sectionQuizMode.style.display = "flex"
+    category.style.display = "block"
+  })
 }
 
 function disappearCategoryButtons() {
-  getElementByQuery(".quiz-category-button").forEach(function (category) {
-    category.style.display = "none";
-  });
+  getElementByQuery(".quiz-category-buttons").forEach(function (category) {
+    category.style.display = "none"
+  })
 }
 
-function clickCategoryButton() {
-  console.log("category");
-  let categoryButton = document.getElementById("category-button");
-  categoryButton.addEventListener("click", function () {
-    appearCategoryButtons();
-    removeDifficultyButtons();
-    console.log("click");
-    difficultyButton.removeAttribute("disabled");
-  });
-}
-
-function clickDiffilcultyButton() {
-  difficultyButton.addEventListener("click", function () {
-    disappearCategoryButtons();
-    addDifficultyButtons();
-    setDifficulty();
-    difficultyButton.setAttribute("disabled", "true");
-  });
+function completeSpan(selection) {
+  let spanCategory = document.getElementById(`span-${selection}`)
+  spanCategory.textContent = localStorage.getItem(selection)
 }
 
 function setCategory() {
-  getElementByQuery(".quiz-category-button").forEach(function (category) {
+  getElementByQuery(".quiz-category-buttons").forEach(function (category) {
     category.addEventListener("click", function () {
-      localStorage.setItem("category", `${category.textContent}`);
-      difficultyButton.style.display = "block";
-      appearStartButton();
-    });
-  });
+      localStorage.setItem("category", `${category.textContent}`)
+      addDifficultyButtons()
+      appearDifficultyButtons()
+      disappearCategoryButtons()
+      completeSpan("category")
+      appearStartButton()
+      setDifficulty()
+    })
+  })
 }
 
 function setDifficulty() {
   getElementByQuery(".quiz-difficulty-buttons").forEach(function (difficulty) {
     difficulty.addEventListener("click", function () {
-      let text = difficulty.querySelector(".quiz-h4");
-      localStorage.setItem("difficulty", `${text.textContent}`);
-      appearStartButton();
-    });
-  });
+      let text = difficulty.querySelector(".quiz-h4")
+      localStorage.setItem("difficulty", `${text.textContent}`)
+      completeSpan("difficulty")
+      appearStartButton()
+    })
+  })
 }
 
 function appearStartButton() {
   if (localStorage.getItem("category") && localStorage.getItem("difficulty")) {
-    getElementByQuery(".start-button")[0].style.display = "flex";
+    getElementByQuery(".start-button")[0].style.display = "flex"
   }
 }
 
 window.addEventListener("load", function () {
+  this.localStorage.clear()
+
   setTimeout(function () {
-    modal.classList.add("show");
-    header.classList.add("show");
-  }, 200);
-  this.localStorage.clear();
-  addCategoryButtons();
-  clickCategoryButton();
-  clickDiffilcultyButton();
-  setDifficulty();
-  setCategory();
-});
+    modal.classList.add("show")
+    header.classList.add("show")
+  }, 200)
+})
