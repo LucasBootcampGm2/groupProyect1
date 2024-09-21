512a8c5a66239c35589d1ff121da5882f5a2612a
thaisgebel <maria.thais.gebel@gmail.com>
2024-09-21 17:14:20 -0300

    feat(answersExplained):modify style, modify colors question bank, change font family

--- resultsScreen/results.js
+++ resultsScreen/results.js
@@ -1,14 +1,26 @@
 import { points } from "../questionBank.js";
+import { colors } from "../questionBank.js";
 
 let crown = document.querySelector(".fa-crown");
 let score = document.querySelector(".score");
 
 let pWrongAnswers = document.getElementById("paragraph-wrong-answers");
 let pCorrectAnswers = document.getElementById("paragraph-correct-answers");
+let backgroundColor = document.querySelector(".background-color");
+
 
 let user = JSON.parse(localStorage.getItem("user"));
 
 let selectedDifficulty = localStorage.getItem("difficulty");
+let selectedCategory = localStorage.getItem("category");
+
+function pageError (){
+  if (localStorage.length === 0){
+    window.location.replace("../notFoundPage/notFoundPage.html");
+  }
+}
+pageError();
+
 
 let correctAnswers = user.answers.filter(function(answer) {
   return answer === "correct";
@@ -23,6 +35,7 @@ let skippedAnswers = user.answers.filter(function(answer) {
 let totalQuestions = correctAnswers + wrongAnswers + skippedAnswers;
 let correctValue = points.valuesCorrect[selectedDifficulty];
 let wrongValue = points.valuesIncorrect[selectedDifficulty];
+
 
 function numberOfQuestions(correct, wrong, skip) {
   document.querySelector(".correct-number").textContent = correct;
@@ -57,10 +70,15 @@ function changeValuePoints() {
   pCorrectAnswers.textContent = `Correct Answers (+${correctValue} pts)`;
   pWrongAnswers.textContent = `Wrong Answers (${wrongValue} pts)`;
 }
+function changeColorCategory() {
+  backgroundColor.style.backgroundColor = colors[selectedCategory];
+}
+
 
 window.addEventListener("load", function () {
   numberOfQuestions(correctAnswers, wrongAnswers, skippedAnswers);
   finalScore(correctAnswers, wrongAnswers);
   showCrown();
   changeValuePoints();
+  changeColorCategory();
 });
