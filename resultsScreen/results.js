2dc660285213113aada140bb66b2bc8a6b66aa06
thaisgebel <maria.thais.gebel@gmail.com>
2024-09-22 14:42:33 -0300

    feat(answersExplained): change style

--- resultsScreen/results.js
+++ resultsScreen/results.js
@@ -67,8 +67,8 @@ function showCrown() {
 }
 
 function changeValuePoints() {
-  pCorrectAnswers.textContent = `Correct Answers (+${correctValue} pts)`;
-  pWrongAnswers.textContent = `Wrong Answers (${wrongValue} pts)`;
+  pCorrectAnswers.textContent = `(+${correctValue} pts)`;
+  pWrongAnswers.textContent = `(${wrongValue} pts)`;
 }
 function changeColorCategory() {
   backgroundColor.style.backgroundColor = colors[selectedCategory];
