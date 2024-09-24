6a1442a997cac88d1f835c040c935e6d0452e46f
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-09-24 11:28:20 -0300

    feat(home.js): add a verification at set the username

--- home/home.js
+++ home/home.js
@@ -1,70 +1,74 @@
-import { questions } from "../questionBank.js"
+import { questions } from "../questionBank.js";
 
-let modal = document.getElementById("modal")
-let button = document.querySelector(".buttonSubmit")
+let modal = document.getElementById("modal");
+let button = document.querySelector(".buttonSubmit");
 
-let header = document.querySelector(".header")
-let selectionH2 = document.querySelector(".quiz-selection-h2")
+let header = document.querySelector(".header");
+let selectionH2 = document.querySelector(".quiz-selection-h2");
 
-let sectionQuizMode = document.getElementById("section-quiz-mode")
-let containerSpans = document.querySelector(".container-spans")
-let footer = document.querySelector(".footer")
+let sectionQuizMode = document.getElementById("section-quiz-mode");
+let containerSpans = document.querySelector(".container-spans");
+let footer = document.querySelector(".footer");
 
-let backCategoryButton = document.getElementById("back-category")
-let backDifficultyButton = document.getElementById("back-difficulty")
+let backCategoryButton = document.getElementById("back-category");
+let backDifficultyButton = document.getElementById("back-difficulty");
 
-let userObject = {}
+let userObject = {};
 
-let categories = Object.keys(questions)
+let categories = Object.keys(questions);
 
-console.log(categories)
+console.log(categories);
 
 function addUserName() {
-  let username = document.getElementById("username")
-  userObject.userName = username.value
-  console.log(userObject)
-  localStorage.setItem("user", JSON.stringify(userObject))
+  let username = document.getElementById("username");
+  if ((!username.value)) {
+    userObject.userName = "unknown";
+  } else {
+    userObject.userName = username.value;
+  }
+  console.log(userObject);
+  localStorage.setItem("user", JSON.stringify(userObject));
 }
 
 button.addEventListener("click", function () {
-  modal.classList.remove("show")
-  header.classList.remove("show")
-  console.log(addUserName())
-  addCategoryButtons()
+  modal.classList.remove("show");
+  header.classList.remove("show");
+  console.log(addUserName());
+  addCategoryButtons();
   setTimeout(function () {
-    main.style.display = "block"
+    main.style.display = "block";
     document
       .querySelectorAll(".quiz-category-buttons")
       .forEach(function (buttons) {
-        buttons.classList.add("show")
-      })
-    modal.style.display = "none"
-    header.style.display = "none"
-    containerSpans.style.display = "flex"
-  }, 1000)
+        buttons.classList.add("show");
+      });
+    modal.style.display = "none";
+    header.style.display = "none";
+    containerSpans.style.display = "flex";
+  }, 1000);
 
-  setCategory()
-  footer.style.display = "flex"
-})
+  setCategory();
+  footer.style.display = "flex";
+});
 
 function saveDifficulties() {
-  let difficulties = {}
+  let difficulties = {};
   categories.forEach(function (category) {
-    difficulties[category] = Object.keys(questions[category])
-  })
-  return difficulties
+    difficulties[category] = Object.keys(questions[category]);
+  });
+  return difficulties;
 }
-let difficulties = saveDifficulties()
-console.log(difficulties)
+let difficulties = saveDifficulties();
+console.log(difficulties);
 
 function getElementByQuery(element) {
-  return document.querySelectorAll(element)
+  return document.querySelectorAll(element);
 }
 
 function createNewClasses(element, classes) {
   classes.forEach(function (newClass) {
-    element.classList.add(newClass)
-  })
+    element.classList.add(newClass);
+  });
 }
 
 function createNewAttributes(element, attributes) {
@@ -72,20 +76,20 @@ function createNewAttributes(element, attributes) {
     element.setAttribute(
       newAttribute.attributeName,
       newAttribute.attributeValue
-    )
-  })
+    );
+  });
 }
 
 function addTextToElement(element, text) {
-  element.textContent = text
+  element.textContent = text;
 }
 
 function createCompleteElement(element, classes, attributes, text) {
-  let newElement = document.createElement(element)
-  createNewClasses(newElement, classes)
-  createNewAttributes(newElement, attributes)
-  addTextToElement(newElement, text)
-  return newElement
+  let newElement = document.createElement(element);
+  createNewClasses(newElement, classes);
+  createNewAttributes(newElement, attributes);
+  addTextToElement(newElement, text);
+  return newElement;
 }
 
 function addCategoryButtons() {
@@ -99,7 +103,7 @@ function addCategoryButtons() {
           attributeValue: `${category.toLowerCase()}-button`,
         },
       ]
-    )
+    );
 
     let newImg = createCompleteElement(
       "img",
@@ -118,24 +122,24 @@ function addCategoryButtons() {
           attributeValue: `${category.toLowerCase()}Img`,
         },
       ]
-    )
+    );
 
     let newH4 = createCompleteElement(
       "h4",
       ["quiz-h4"],
       [""],
       category.toUpperCase()
-    )
-    newButton.append(newImg)
-    newButton.append(newH4)
-    sectionQuizMode.append(newButton)
-  })
+    );
+    newButton.append(newImg);
+    newButton.append(newH4);
+    sectionQuizMode.append(newButton);
+  });
 }
 
 function addDifficultyButtons() {
-  let categorySelected = localStorage.getItem("category")
-  let categoryDifficulties = Object.values(difficulties[categorySelected])
-  console.log(difficulties[categorySelected])
+  let categorySelected = localStorage.getItem("category");
+  let categoryDifficulties = Object.values(difficulties[categorySelected]);
+  console.log(difficulties[categorySelected]);
 
   categoryDifficulties.forEach(function (categoryDifficulty) {
     let newButton = createCompleteElement(
@@ -147,7 +151,7 @@ function addDifficultyButtons() {
           attributeValue: `${categoryDifficulty}-button`,
         },
       ]
-    )
+    );
 
     let newImg = createCompleteElement(
       "img",
@@ -166,113 +170,113 @@ function addDifficultyButtons() {
           attributeValue: `${categoryDifficulty}Img`,
         },
       ]
-    )
+    );
 
-    newButton.append(newImg)
+    newButton.append(newImg);
 
     let newH4 = createCompleteElement(
       "h4",
       ["quiz-h4"],
       [],
       categoryDifficulty.toUpperCase()
-    )
+    );
 
-    newButton.append(newH4)
+    newButton.append(newH4);
 
-    sectionQuizMode.append(newButton)
-  })
+    sectionQuizMode.append(newButton);
+  });
 }
 
 function appearDifficultyButtons() {
   getElementByQuery(".quiz-difficulty-buttons").forEach(function (difficulty) {
-    difficulty.style.display = "block"
-  })
+    difficulty.style.display = "block";
+  });
 }
 
 function disappearDifficultyButtons() {
   getElementByQuery(".quiz-difficulty-buttons").forEach(function (difficulty) {
-    difficulty.style.display = "none"
-  })
+    difficulty.style.display = "none";
+  });
 }
 
 function removeDifficultyButtons() {
   getElementByQuery(".quiz-difficulty-buttons").forEach(function (difficulty) {
-    difficulty.remove()
-  })
+    difficulty.remove();
+  });
 }
 
 function appearCategoryButtons() {
   getElementByQuery(".quiz-category-buttons").forEach(function (category) {
-    sectionQuizMode.style.display = "flex"
-    category.style.display = "block"
-  })
+    sectionQuizMode.style.display = "flex";
+    category.style.display = "block";
+  });
 }
 
 function disappearCategoryButtons() {
   getElementByQuery(".quiz-category-buttons").forEach(function (category) {
-    category.style.display = "none"
-  })
+    category.style.display = "none";
+  });
 }
 
 function completeSpan(selection) {
-  let spanCategory = document.getElementById(`span-${selection}`)
-  spanCategory.textContent = localStorage.getItem(selection)
+  let spanCategory = document.getElementById(`span-${selection}`);
+  spanCategory.textContent = localStorage.getItem(selection);
 }
 
 function setCategory() {
   getElementByQuery(".quiz-category-buttons").forEach(function (category) {
     category.addEventListener("click", function () {
-      localStorage.setItem("category", `${category.textContent}`.toLowerCase())
-      removeDifficultyButtons()
-      addDifficultyButtons()
-      appearDifficultyButtons()
-      disappearCategoryButtons()
-      completeSpan("category")
-      appearStartButton()
-      backButtonsClick()
-      setDifficulty()
-      backDifficultyButton.style.display = "none"
-      backCategoryButton.style.display = "flex"
-      selectionH2.textContent = "Select Difficulty"
-    })
-  })
+      localStorage.setItem("category", `${category.textContent}`.toLowerCase());
+      removeDifficultyButtons();
+      addDifficultyButtons();
+      appearDifficultyButtons();
+      disappearCategoryButtons();
+      completeSpan("category");
+      appearStartButton();
+      backButtonsClick();
+      setDifficulty();
+      backDifficultyButton.style.display = "none";
+      backCategoryButton.style.display = "flex";
+      selectionH2.textContent = "Select Difficulty";
+    });
+  });
 }
 
 function setDifficulty() {
   getElementByQuery(".quiz-difficulty-buttons").forEach(function (difficulty) {
     difficulty.addEventListener("click", function () {
-      let text = difficulty.querySelector(".quiz-h4")
-      localStorage.setItem("difficulty", `${text.textContent}`.toLowerCase())
-      completeSpan("difficulty")
-      appearStartButton()
-      backDifficultyButton.style.display = "none"
-      backCategoryButton.style.display = "flex"
-    })
-  })
+      let text = difficulty.querySelector(".quiz-h4");
+      localStorage.setItem("difficulty", `${text.textContent}`.toLowerCase());
+      completeSpan("difficulty");
+      appearStartButton();
+      backDifficultyButton.style.display = "none";
+      backCategoryButton.style.display = "flex";
+    });
+  });
 }
 
 function appearStartButton() {
   if (localStorage.getItem("category") && localStorage.getItem("difficulty")) {
-    getElementByQuery(".start-button")[0].style.display = "flex"
+    getElementByQuery(".start-button")[0].style.display = "flex";
   }
 }
 
 function backButtonsClick() {
   backCategoryButton.addEventListener("click", function () {
-    disappearDifficultyButtons()
-    appearCategoryButtons()
-    backDifficultyButton.style.display = "flex"
-    backCategoryButton.style.display = "none"
-    selectionH2.textContent = "Select Category"
-  })
+    disappearDifficultyButtons();
+    appearCategoryButtons();
+    backDifficultyButton.style.display = "flex";
+    backCategoryButton.style.display = "none";
+    selectionH2.textContent = "Select Category";
+  });
 
   backDifficultyButton.addEventListener("click", function () {
-    disappearCategoryButtons()
-    appearDifficultyButtons()
-    backDifficultyButton.style.display = "none"
-    backCategoryButton.style.display = "flex"
-    selectionH2.textContent = "Select Difficulty"
-  })
+    disappearCategoryButtons();
+    appearDifficultyButtons();
+    backDifficultyButton.style.display = "none";
+    backCategoryButton.style.display = "flex";
+    selectionH2.textContent = "Select Difficulty";
+  });
 }
 
 function setLeaderboard() {
@@ -292,12 +296,13 @@ function setLeaderboard() {
 }
 
 window.addEventListener("load", function () {
-  backButtonsClick()
-  if (!this.localStorage.getItem("leaderboard")){
+  backButtonsClick();
+  if (!this.localStorage.getItem("leaderboard")) {
     localStorage.setItem("leaderboard", JSON.stringify(setLeaderboard()));
   }
   setTimeout(function () {
-    modal.classList.add("show")
-    header.classList.add("show")
-  }, 200)
-})
+    modal.classList.add("show");
+    header.classList.add("show");
+  }, 200);
+
+});
